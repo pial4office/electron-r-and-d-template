2dbd35e253e2c9cd4b94c255290205138c823a7b
Md. Pial Ahamed <pial4office@gmail.com>
2022-06-27 17:44:48 +0600

    Initialize electron R&D template

---
+++ public/main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// 𝖈𝖔𝖓𝖋𝖎𝖌
+const appConfig = {
+  isDev: true,
+  devURL: 'http://localhost:3000',
+  prodURL: path.join(__dirname, './public/index.html'),
+};
+
+// 𝖆𝖕𝖕
+function createWindow() {
+  const appWindow = new BrowserWindow({ show: false });
+  appWindow.maximize();
+
+  appWindow.loadURL(appConfig.isDev ? appConfig.devURL : appConfig.prodURL);
+
+  appConfig.isDev && appWindow.webContents.openDevTools();
+}
+
+// 𝖆𝖕𝖕 𝖑𝖎𝖘𝖙𝖊𝖓𝖊𝖗
+app.whenReady().then(() => {
+  createWindow();
+});
